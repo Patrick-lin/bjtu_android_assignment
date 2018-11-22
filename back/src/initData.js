ee14928bf94cf881284435cd7beb1aca95d7f002
Patrick <shantry@hotmail.fr>
2018-11-23 02:24:52 +0800

    News initData

--- back/src/initData.js
+++ back/src/initData.js
@@ -1,6 +1,6 @@
 import faker from 'faker';
 
-import { Trainers, Classes } from './db';
+import { Trainers, Classes, News } from './db';
 import { arrayElements } from './utils';
 
 const createTrainer = () => ({
@@ -8,14 +8,14 @@ const createTrainer = () => ({
   lastName: faker.name.lastName(),
   age: faker.random.number({ min: 20, max: 35 }),
   cover: faker.random.arrayElement([
-    "https://www.randomaddressgenerator.com/media/face/male36.jpg",
-    "https://www.randomaddressgenerator.com/media/face/male67.jpg",
-    "https://www.randomaddressgenerator.com/media/face/female72.jpg",
-    "https://www.randomaddressgenerator.com/media/face/male44.jpg",
-    "https://www.randomaddressgenerator.com/media/face/female40.jpg",
-    "https://www.randomaddressgenerator.com/media/face/male50.jpg",
-    "https://www.randomaddressgenerator.com/media/face/male32.jpg",
-    "https://www.randomaddressgenerator.com/media/face/female82.jpg",
+    'https://www.randomaddressgenerator.com/media/face/male36.jpg',
+    'https://www.randomaddressgenerator.com/media/face/male67.jpg',
+    'https://www.randomaddressgenerator.com/media/face/female72.jpg',
+    'https://www.randomaddressgenerator.com/media/face/male44.jpg',
+    'https://www.randomaddressgenerator.com/media/face/female40.jpg',
+    'https://www.randomaddressgenerator.com/media/face/male50.jpg',
+    'https://www.randomaddressgenerator.com/media/face/male32.jpg',
+    'https://www.randomaddressgenerator.com/media/face/female82.jpg',
   ]),
 });
 
@@ -51,7 +51,7 @@ const createClass = ({
   title,
   tagLine,
   rank,
-  
+
   availableTrainerIds,
   choosableTrainer,
   nbChoosableTrainer,
@@ -106,7 +106,58 @@ const initClasses = async () => {
   ]);
 }
 
+const createNews = ({
+  title = faker.lorem.sentence(4),
+  text = faker.lorem.paragraph(50),
+  date,
+  tagLine = faker.lorem.sentence(10),
+  cover = faker.random.arrayElement([
+    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=754226097,347722291&fm=15&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2642340674,2087553279&fm=15&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=298546301,221452502&fm=15&gp=0.jpg',
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2720094953,3884838007&fm=15&gp=0.jpg',
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3474596885,3589851943&fm=15&gp=0.jpg',
+    'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=332834531,2740295829&fm=15&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2221104227,1880545130&fm=15&gp=0.jpg',
+    'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=830621634,2797568601&fm=26&gp=0.jpg',
+    'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3736244080,2841172467&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3588909357,1860370031&fm=26&gp=0.jpg'
+  ]),
+} = {}) => ({
+  title,
+  text,
+  date,
+  tagLine,
+  cover,
+});
+
+
+function* makeDateGenerator() {
+  let date = new Date();
+  yield date;
+
+  while (true) {
+    date -= faker.random.number({ min: 6, max: 96 }) * 60 * 60 * 1000,
+    yield date
+  }
+}
+
+const createNewsBatch = (nb) => {
+  const dateGenerator = makeDateGenerator();
+  const res = [];
+  for (let i = 0; i < nb; i++) {
+    res.unshift(createNews({ date: dateGenerator.next().value }));
+  }
+  console.log(res);
+  return res;
+}
+
+const initNews = () => {
+  return News.insert(createNewsBatch(120));
+}
+
 export default async () => {
   await initTrainers();
   await initClasses();
+  await initNews();
 }
